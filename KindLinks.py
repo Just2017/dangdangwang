67dab1d3177801089722585a7f86a13effd299ab
Just <hackie_2015@163.com>
2017-04-02 15:59:06 +0800

    Add files via upload

---
+++ KindLinks.py
@@ -0,0 +1,38 @@
+#当当网商品种类链接，获取不同种类的所有图书
+
+from bs4 import BeautifulSoup
+
+class _FirstPageLinkToGetUrl():
+    def __init__(self,opener):
+        self.opener=opener
+        self.url="http://category.dangdang.com/?ref=www-0-C"
+
+
+    def getDifferentSeriesBookUrl(self):
+        html=self.opener.open(self.url).read().decode("gbk")
+
+        soup=BeautifulSoup(html,"html.parser")
+        #类别
+        LB = []
+        # 字典存储小类别对应的URL
+        #dictUrl = {}
+        #
+        temp=0
+        listUrl=[]
+        count=[]
+        #outside  ---外层的div
+        #_li      ---li层
+        for outsideDiv in soup.find("div", class_="classify_books", id="floor_1").find_all("div", class_="classify_kind"):
+            LB.append(outsideDiv.div.a.string)
+            temp=0
+            dictUrl={}
+            for _li in outsideDiv.find("ul").find_all("li"):
+                if _li.a.string == "更多":
+                    continue
+                else:
+                   # print(s.a.get("href"), s.a.string)
+                    temp+=1
+                    dictUrl[_li.a.string] = _li.a.get("href")
+            count.append(temp)
+            listUrl.append(dictUrl)
+        return listUrl,LB
